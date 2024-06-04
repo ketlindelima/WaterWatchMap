b04f8e41e42556820831ef06d0bf2c0837ec79f9
Ketlin de lima <ketlin@senocwb.com>
2024-06-04 17:13:18 -0300

    Initial home page

--- obj/Debug/net8.0/WaterWatchMap.AssemblyInfo.cs
+++ obj/Debug/net8.0/WaterWatchMap.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WaterWatchMap")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18af4db8759bfa8bbfa86c29afaff003bb87fd15")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+738b4aed14bd09d1106692a69c2134f600bed5ed")]
 [assembly: System.Reflection.AssemblyProductAttribute("WaterWatchMap")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WaterWatchMap")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
